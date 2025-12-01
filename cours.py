edbdd751ac1b6c92cada8dadd5749cbda920a557
huntjohns045-tech <huntjohns045@gmail.com>
2025-12-01 20:31:12 +0100

    first

---
+++ cours.py
@@ -0,0 +1,137 @@
+#les commentaires
+#couleur="orange"
+
+'''
+couleur="orange"
+age=15
+print("ta couleur est:",couleur)
+'''
+"""
+couleur=6
+print(couleur)
+"""
+
+#les sorties en python
+
+#les sorties de texte
+print("bonjour")
+
+print('bonjour')
+
+print("hello",end=' ') # end permet dafficher linstruction daffichage suivante sans toute fois aller a la ligne
+print("holla")
+
+print("bonjour");print("les");print("amis") # le separateur (;) permet davoir plusieurs instructions daffichage sur la meme ligne
+
+# les sorties de nombres 
+print(9)
+print(12)
+print(3+4)
+print("jai",12,"ans")
+
+# les variables
+couleur="orange" #chaine de caracteres
+age=12 # entier
+moyenne=18,5 # reel
+print(couleur);print(age);print(moyenne)
+
+# recuperer les types dune variable
+print(type(couleur)) # type est une fonction qui prend en parametre une variable et retourne son type 
+print(type(age))
+print(type(moyenne))
+
+# les noms des variables
+jour75=15
+le_match_du_jour=0
+chaine="yo"
+SOMME='notes'
+_eleves_=12
+monNom="johann"
+a=6
+A=6
+
+# mutiples declaration de variables
+a,b,c=1,2,3 
+
+fruit=("orange","citron","papaye","mangue") # il sagit la dun tuple car on note lutilisation de parentheses()
+jour_de_semaine=["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"] #il sagit la dune liste car lon note la presence de crochets[]
+ecriture={"crayon","stylo","support","lancre","idee","telephone"}# il sagit la dun ensemble ou set car lon note la presence daccolades{}
+dict_etudiant={"matricule" :12365,"age" :15,"nom" :"jean","prenom" :"piere"} # il sagit la dun dictionnaire qui permet de regrouper des donnees en cles:valeurs et delimitee par des accolades  
+
+print(jour_de_semaine[6])
+
+variable="mechant"
+constante="grnad mechant"
+print(variable,constante)
+print(variable+constante)
+
+#print("je suis age de" +12 ) # laffichage de variables avec + ne marchent que si kes variables sont de memes type
+
+# les types en python
+
+#types texte
+#chaine de_caractere 
+couleur="jaune"
+print(type(couleur)) #fonction de formatage pour une chaine de caractere est str()
+
+# les nombres
+age = 25 #fonction de formatage pour un entier est int() N Z
+moyenne = 18.5 #fonction de formatage pour un floatant ou reel est float() D Q R
+complex=14-14j #fonction de formatage pour un complex est complex() C
+print(type(age));print(type(moyenne));print(type(complex));print("la partie reelle est ",complex.real,"la partie imaginaire est ",complex.imag)
+
+# les booleens
+nom=True  #fonction de formatage pour un booleen est bool()
+print(type(nom))
+
+#les types sequencieles
+
+# les listes (jour_de_semaine) #fonction de formatage pour une liste  est list()
+# les tuples(fruit) #fonction de formatage pour un tuple est tuple()
+# les range(range(10)) #fonction de formatage pour un range est range()
+
+jour=range(7)
+
+print(type(jour_de_semaine),type(fruit),type(jour))
+ 
+# les None
+ecole=None  #fonction de formatage pour un None est NoneType()
+print(type(None))
+
+# les structures conditionnelles
+#if():
+
+#if():
+#else:
+
+#if():
+#elif():
+#else:
+
+
+'''
+les operateurs conditionnels 
+== :permet de montrer que deux nombres sont egaux
+!= :permet de mntrer que deux nombres sont differents
+< :permet de montrer quun nombre est inferieur a un autre  
+> :permet de montrer quun nombre est superieur a un autre
+<= :permet de montrer quun nombre est inferieur ou egale a un autre
+>= :permet de montrer quun nombre est superieur ou egale a un autre
+les opeateurs logiques
+or :condition vraie quand au moins une condition est vraie
+and :condition vraie quand toutes les conditionss sont vraies
+not :condition vraie quand condition fausse
+'''
+
+#l etat de passage
+if():
+    pass # etat de passage(termine le if)
+
+# structure iterrative
+
+
+
+
+
+
+
---
+++ equationsecondegre.py
@@ -0,0 +1,57 @@
+#resolution dune equation de type f(x)=ax^2+bx+c
+
+"""
+import math
+
+a=3
+b=4
+c=5
+delta=-b*b-4*a*c
+if(delta>0):
+   
+    print("il existe de solutions")
+    x1=(-b-math.sqrt(delta))/2*a
+    x2=(-b+math.sqrt(delta))/2*a
+
+    print("x1=",x1);print("x2=",x2)
+
+elif(delta==0):    
+    print("il existe une solution")
+    x=-b/2*a
+
+    print("x=","x")
+
+else:
+    print("il existe deux solutions")
+    x1= (-b-(math.sqrt(-delta)))/2*a
+    x2= (-b+(math.sqrt(-delta)))/2*a
+
+    print("x1=",x1);print("x2=",x2)
+"""
+
+nom,prenom=input("entrez votre nom  ") ,input("entrez votre prenom ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
